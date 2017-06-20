3184d0b8a5057676ef67f47f5ebea998a60dcf54
stvkre <kiariemburuh@gmail.com>
2017-06-20 11:04:45 +0300

    intents -add on click listener to the requestRide button to enable user to navigate to the next activity

--- app/src/main/java/com/example/stephen/myriderke/MainActivity.java
+++ app/src/main/java/com/example/stephen/myriderke/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.stephen.myriderke;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mRequestRideButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRequestRideButton = (Button) findViewById(R.id.requestRideButton);
+        mRequestRideButton.setOnClickListener(new View.OnClickListener() {
+
+            // intents to navigate to next activity
+            @Override
+
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RiderActivity.class);
+                startActivity(intent);
+            }
+        })
     }
 }
