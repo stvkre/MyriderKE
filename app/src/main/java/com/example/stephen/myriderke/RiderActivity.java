795a93a239bdc92add675735df1c1ed2edcac226
stvkre <kiariemburuh@gmail.com>
2017-06-21 18:24:47 +0300

    Pick up location -Add geocoder function to render the map to the specified location set by the user

--- app/src/main/java/com/example/stephen/myriderke/RiderActivity.java
+++ app/src/main/java/com/example/stephen/myriderke/RiderActivity.java
@@ -1,14 +1,22 @@
 package com.example.stephen.myriderke;
 
+import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.identity.intents.Address;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
 
 public class RiderActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -47,5 +55,22 @@ public class RiderActivity extends AppCompatActivity implements OnMapReadyCallba
         LatLng ll = new LatLng(lat, lng);
         CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
         mGoogleMap.moveCamera(update);
+    }
+
+    public void geolocate(View view) throws IOException {
+        EditText et = (EditText) findViewById(R.id.editText1);
+        String location = et.getText().toString();
+
+        Geocoder gc = new Geocoder(this);
+        List<android.location.Address> list =  gc.getFromLocationName(location, 3);
+        android.location.Address address = list.get(0);
+        String locality = address.getLocality();
+
+        Toast.makeText(this, locality, Toast.LENGTH_LONG).show();
+
+        double lat = address.getLatitude();
+        double lng = address.getLongitude();
+        goToLocationZoom(lat, lng, 19);
+
     }
 }
