b5203a7aa92bbef125b47936be1f49c8f11990da
stvkre <kiariemburuh@gmail.com>
2017-06-21 11:23:01 +0300

    Map Fragment -Fragment successfully returns Google Maps once the Request Ride button is pressed

--- app/src/main/java/com/example/stephen/myriderke/RiderActivity.java
+++ app/src/main/java/com/example/stephen/myriderke/RiderActivity.java
@@ -3,11 +3,29 @@ package com.example.stephen.myriderke;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RiderActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class RiderActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap mGoogleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rider);
+        initMap();
+
+    }
+
+    private void initMap() {
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
     }
 }
