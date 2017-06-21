42e9add3aa36b5095fcdb67f297d7fe551ef8dad
stvkre <kiariemburuh@gmail.com>
2017-06-21 11:40:19 +0300

    Map -refactored map to get default location once the application is launched

--- app/src/main/java/com/example/stephen/myriderke/RiderActivity.java
+++ app/src/main/java/com/example/stephen/myriderke/RiderActivity.java
@@ -3,9 +3,12 @@ package com.example.stephen.myriderke;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
 public class RiderActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -23,9 +26,19 @@ public class RiderActivity extends AppCompatActivity implements OnMapReadyCallba
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
         mapFragment.getMapAsync(this);
     }
+// initmap functions calls the Googlemap method to display the map on the fragment
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
+
+        // getting default location once map is launched
+        goToLocation(-1.300404,36.7848483);
+    }
+
+    private void goToLocation(double lat, double lng) {
+        LatLng ll = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLng(ll);
+        mGoogleMap.moveCamera(update);
     }
 }
