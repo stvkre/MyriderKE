3c7c1bb3b0ab2e51562b1c4df5a70534d39e9abc
stvkre <kiariemburuh@gmail.com>
2017-06-22 18:56:32 +0300

    Main Activity -changed header text font. -adjusted button size

--- app/src/main/java/com/example/stephen/myriderke/MainActivity.java
+++ app/src/main/java/com/example/stephen/myriderke/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.stephen.myriderke;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button mRequestRideButton;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        Typeface carroisFont = Typeface.createFromAsset(getAssets(), "fonts/CarroisGothic-Regular.ttf");
+        textView.setTypeface(carroisFont);
         mRequestRideButton = (Button) findViewById(R.id.requestRideButton);
         mRequestRideButton.setOnClickListener(new View.OnClickListener() {
 
